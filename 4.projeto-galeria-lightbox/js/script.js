fb0db762226133345f464e54dcc3a5969298f0f1
leirisson <leirissonsouza99@gmail.com>
2024-08-20 11:59:40 -0400

    finalização de ligthbox de imagens
    finalização de ligthbox de imagens

---
+++ 4.projeto-galeria-lightbox/js/script.js
@@ -0,0 +1,20 @@
+// selecionando os items
+const galeria = document.querySelectorAll(".galeria-item")
+const ligth_box = document.querySelector(".lightbox")
+const light_img = document.querySelector(".lightbox-image")
+const light_fechar = document.querySelector(".lightbox-close")
+
+
+// função de abrir o llightbox
+
+galeria.forEach((item)=> {
+    item.addEventListener("click", () => {
+        const img_url = item.querySelector(".galeria-img").getAttribute("data-src")
+        light_img.setAttribute("src", img_url)
+        ligth_box.style.display = "flex"
+    })
+})
+
+light_fechar.addEventListener("click", () => {
+    ligth_box.style.display = "none"
+})
